2e30a3ed5416e856e605c1f4bc1ffe74880a0776
NightFall-Dev <23613531+NightFall-Dev@users.noreply.github.com>
2022-09-14 23:42:28 +0800

    T

--- Files/NGROK-CFG-GEN.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo region: ap>>ngrok.yml
-echo tunnels:>>ngrok.yml
-echo     default:>>ngrok.yml
-echo         proto: tcp>>ngrok.yml
-echo         addr: 3389>>ngrok.yml
-echo         cidr-allow: 0.0.0.0/32>>ngrok.yml
