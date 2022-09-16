cca3ea2c97c60e9c48cabbf159d3ecd8cd919545
NightFall-Dev <23613531+NightFall-Dev@users.noreply.github.com>
2022-09-16 15:36:59 +0800

    Update NGROK-AP.bat

--- Files/NGROK-AP.bat
+++ Files/NGROK-AP.bat
@@ -4,5 +4,5 @@ echo tunnels:>>ngrok.yml
 echo     default:>>ngrok.yml
 echo         proto: tcp>>ngrok.yml
 echo         addr: 3389>>ngrok.yml
-copy "C:\Users\runneradmin\AppData\Local\ngrok.yml" "C:\Windows\System32\"
+copy "ngrok.yml" "C:\Windows\System32\"
 nssm install ngrok C:\Windows\System32\ngrok.exe start --all --config ngrok.yml
