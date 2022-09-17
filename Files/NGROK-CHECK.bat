2d14cc8656990a5c60c0ec20e837c344e0fee4d2
NightFall-Dev <23613531+NightFall-Dev@users.noreply.github.com>
2022-09-17 11:04:06 +0800

    Update NGROK-CHECK.bat

--- Files/NGROK-CHECK.bat
+++ Files/NGROK-CHECK.bat
@@ -5,6 +5,7 @@ echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Can't get NGROK tunnel, be sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM still running: https://dashboard.ngrok.com/status/tunnels " 
 echo User: runneradmin
 echo Pass: gk3Duvcpw72zSOo
+sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-sc config Audiosrv start=auto >nul
+net user installer /delete
 ping -n 10 127.0.0.1 >nul
